b5881159bc16c0b59dff6dfabec28f98688ff746
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2024-01-20 21:38:46 +0600

    productApi updated

--- src/Redux/api/productApi.ts
+++ src/Redux/api/productApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const productApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://tech-net-backend.vercel.app/',
+    baseUrl: `${process.env.BaseUrl}`,
   }),
   endpoints: (builder) => ({
     getProducts: builder.query({
